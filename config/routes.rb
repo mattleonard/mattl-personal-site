6ee85dbd556960ede226f3a67213d478fd7de027
mattleonard <matthew.leonard@colorado.edu>
2015-06-18 18:10:36 -0700

    Styling tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :about_mes
-  resources :portfolios
+  get '/portfolio', to: 'portfolios#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
